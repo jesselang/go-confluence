903715fabf48b0589dac8cf12f13dc6be049da9e
Mitchell Then <Mitchell.Then@VERITAS.com>
2018-11-30 13:12:33 -0600

    Add image support

---
+++ attachment.go
@@ -0,0 +1,144 @@
+package confluence
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Attachment struct {
+	ID string `json:"id,omitempty"`
+	Title string `json:"title,omitempty"`
+	Links struct {
+		Download string `json:"download,omitempty"`
+	} `json:"_links,omitempty"`
+}
+
+type AttachmentResults struct {
+	ResultPagination
+	Results []Attachment `json:"results"`
+}
+
+func (w *Wiki) getAttachmentByFilenameEndpoint(contentID string, filename string) (*url.URL, error) {
+	return url.ParseRequestURI(w.endPoint.String() + "/content/" + contentID + "/child/attachment?filename=" + filename)
+}
+
+func (w *Wiki) createAttachmentEndpoint(contentID string) (*url.URL, error) {
+	return url.ParseRequestURI(w.endPoint.String() + "/content/" + contentID + "/child/attachment")
+}
+
+func (w *Wiki) updateAttachmentEndpoint(contentID string, attachmentID string) (*url.URL, error) {
+	return url.ParseRequestURI(w.endPoint.String() + "/content/" + contentID + "/child/attachment/" + attachmentID + "/data")
+}
+
+func (w *Wiki) GetAttachment(contentID string, filename string) (*AttachmentResults, error) {
+	endpoint, err := w.getAttachmentByFilenameEndpoint(contentID, filename)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", endpoint.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := w.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var results AttachmentResults
+	err = json.Unmarshal(res, &results)
+	if err != nil {
+		return nil, err
+	}
+
+	return &results, nil
+}
+
+func (w *Wiki) GetAttachmentData(contentID string, filename string) ([]byte, error) {
+	result, err := w.GetAttachment(contentID, filename)
+	if err != nil {
+		return nil, err
+	}
+
+	attachmentDataURL, err := url.ParseRequestURI(strings.Replace(w.endPoint.String(), "/rest/api", "", -1) + result.Results[0].Links.Download)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", attachmentDataURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := w.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (w *Wiki) CreateAttachment(contentID string, path string) (*AttachmentResults, error) {
+	endpoint, err := w.createAttachmentEndpoint(contentID)
+	if err != nil {
+		return nil, err
+	}
+	return w.createOrUpdateAttachment(endpoint, path)
+}
+
+func (w *Wiki) UpdateAttachment(contentID string, path string, attachmentID string) (*AttachmentResults, error) {
+	endpoint, err := w.updateAttachmentEndpoint(contentID, attachmentID)
+	if err != nil {
+		return nil, err
+	}
+	return w.createOrUpdateAttachment(endpoint, path)
+}
+
+func (w *Wiki) createOrUpdateAttachment(endpoint *url.URL, path string) (*AttachmentResults, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", filepath.Base(path))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = io.Copy(part, file)
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", endpoint.String(), body)
+	req.Header.Add("X-Atlassian-Token", "nocheck")
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := w.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var results AttachmentResults
+	err = json.Unmarshal(res, &results)
+	if err != nil {
+		return nil, err
+	}
+
+	return &results, nil
+}
